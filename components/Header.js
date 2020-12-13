1188709bf5624984f614eed3164fa91cacf93b5f
matsfranco <matsfranco@outlook.com.br>
2020-12-13 10:27:38 -0300

    Introdução de CSS Global e style-jsx

--- components/Header.js
+++ components/Header.js
@@ -1,14 +1,13 @@
-const headerStyle = {
-    backgroundColor: "blue",
-    color: "white",
-    width: "100%",
-    height: "50px"
-  };
-  
-  const Header = () => (
-    <div className="Header" style={headerStyle}>
-      HEADER
-    </div>
-  );
-  
-  export default Header;
+const Header = () => (
+  <div className="Header">
+    HEADER
+    <style jsx>{`
+      background-color: blue;
+      color: white;
+      width: 100%;
+      height: 50px;
+    `}</style>
+  </div>
+);
+
+export default Header;
--- components/NavBar.js
+++ components/NavBar.js
@@ -1,14 +1,13 @@
-const navBarStyle = {
-    backgroundColor: "red",
-    color: "white",
-    width: "100%",
-    height: "60px"
-  };
-  
-  const NavBar = () => (
-    <div className="NavBar" style={navBarStyle}>
-      NAVBAR
-    </div>
-  );
-  
-  export default NavBar;
+const NavBar = () => (
+  <div className="NavBar">
+    NAVBAR
+    <style jsx>{`
+      background-color: red;
+      color: white;
+      width: 100%;
+      height: 60px;
+    `}</style>
+  </div>
+);
+
+export default NavBar;
