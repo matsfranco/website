6a61f6630cf1003924e629cd37140c784bc41883
matsfranco <matsfranco@outlook.com.br>
2021-01-17 02:55:17 -0300

    Inclusão do Botão de Home e remoção de Link

--- config/buttons.js
+++ config/buttons.js
@@ -4,29 +4,35 @@ import AccountCircleIcon from '@material-ui/icons/AccountCircle';
 import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
 import BookIcon from '@material-ui/icons/Book';
 import MailIcon from '@material-ui/icons/Mail';
+import HomeIcon from '@material-ui/icons/Home';
 import LinkIcon from '@material-ui/icons/Link';
 
 const navButtons = [
     {
-        label: "Perfil",
+        label: "Home",
+        labelEn: "Home",
+        path: "/",
+        icon: <HomeIcon/>
+    },{
+        label: "Sobre",
+        labelEn: "About",
         path: "/profile",
         icon: <AccountCircleIcon/>
     },{
         label: "Portfolio",
+        labelEn: "Portfolio",
         path: "/portfolio",
         icon: <BusinessCenterIcon/>
     },{
         label: "Blog",
+        labelEn: "Blog",
         path: "/blog",
         icon: <BookIcon/>
     },{
         label: "Contato",
+        labelEN: "Contact",
         path: "/contato",
         icon: <MailIcon/>
-    },{
-        label: "Links",
-        path: "/link-tree",
-        icon: <LinkIcon/>
     }
 ];
 
