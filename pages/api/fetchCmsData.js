a1f00395a098cb14b2574337b7b2307d06908b6e
matsfranco <matsfranco@outlook.com.br>
2021-01-24 19:06:15 -0300

    Otimização na formação da URL na classe de Fetch

--- pages/api/fetchCmsData.js
+++ pages/api/fetchCmsData.js
@@ -1,23 +1,14 @@
 async function getCmsData(request, response) {
 
-    let articles = await getCmsByContentType('article');
-    let projects = await getCmsByContentType('project');
-    let experiences = await getCmsByContentType('experience');
-    let institutionsCompanies = await getCmsByContentType('institution-company');
-    let skills = await getCmsByContentType('skill'); 
-    let technologies = await getCmsByContentType('technology');
-    let publications = await getCmsByContentType('publication');
-    let certifications = await getCmsByContentType('certification');
-
     response.json({
-        articles: articles.data,
-        certifications: certifications.data,
-        experiences: experiences.data,
-        institutionsCompanies: institutionsCompanies.data,
-        projects: projects.data,
-        publications: publications.data,
-        skills: skills.data,
-        technologies: technologies.data,
+        articles: await getCmsByContentType('article'),
+        certifications: await getCmsByContentType('certification'),
+        experiences: await getCmsByContentType('experience'),
+        institutionsCompanies: await getCmsByContentType('institution-company'),
+        projects:  await getCmsByContentType('project'),
+        publications: await getCmsByContentType('publication'),
+        skills: await getCmsByContentType('skill'),
+        technologies: await getCmsByContentType('technology')
     })
 
 }
@@ -32,7 +23,7 @@ async function getCmsByContentType(contentType) {
         }
     }
 
-    const cmsResponse = await fetch("https://api.elegantcms.io/api/v1/contents?filter[type]="+contentType+"&filter[status]=live", fetchParams); 
+    const cmsResponse = await fetch(buildEndPointUrl(contentType), fetchParams); 
     const content = await cmsResponse.json();
 
     console.log((content.data).length);
@@ -40,11 +31,11 @@ async function getCmsByContentType(contentType) {
         console.log('['+content.data[i].id+'] '+content.data[i].attributes.title);
     }
 
-    return content;
+    return content.data;
 }
 
 function buildEndPointUrl(contentType) {
-    let baseUrl = "https://api.elegantcms.io/api/v1/contents?filter[type]="+contentType+"&filter[status]=live";
+    return  "https://api.elegantcms.io/api/v1/contents?filter[type]="+contentType+"&filter[status]=live";
 }
 
 export default getCmsData
