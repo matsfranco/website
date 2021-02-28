fed64f4245bd36ea567ef4bae4c3841281c22d36
matsfranco <matsfranco@outlook.com.br>
2021-02-28 02:03:12 -0300

    inclusão da tag HEAD

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -2,12 +2,21 @@ import Background from '../Background/index.js';
 import Header from '../Header/index.js';
 import NavBar from '../HNavigationBar/index.js';
 import navItems from '../../shared/NavigationItems.js';
-
+  
+import Head from "next/head";
 export default function Layout () {
     
     return(
-        <Background>
-            <Header></Header>
-        </Background>      
+
+            <Background> 
+                <Head>
+                    <title>Mateus Franco</title>
+                    <meta name="viewport" content="width=device-width, initial-scale=1" />
+                    <meta charSet="utf-8" />
+                </Head>
+                <Header></Header>
+            </Background>    
+
+             
     );
 }
