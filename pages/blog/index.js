fc27b1d94353e336831deb6bfc394f282f3eb451
matsfranco <matsfranco@outlook.com.br>
2021-02-28 02:09:00 -0300

    Diferenciação do conteúdo base

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -3,7 +3,7 @@ import Layout from '../../src/components/Layout/index.js';
 export default function Home() {
     return(
         <Layout>
-            <div>Home</div>
+            <div>Blog</div>
         </Layout>
     );
 }
