de25afef27acdfd4a307d10a08450cc725111889
matsfranco <matsfranco@outlook.com.br>
2021-05-23 11:42:00 -0300

    Inclusão de pontos de quebra no grid

--- pages/resume/index.js
+++ pages/resume/index.js
@@ -4,7 +4,14 @@ import ExperienceSection from '../../src/components/ExperienceSection/index.js';
 import LanguageSection from '../../src/components/LanguageSection/index.js';
 import EducationSection from '../../src/components/EducationSection/index.js';
 import SkillSection from '../../src/components/SkillSection/index.js';
+import { makeStyles } from '@material-ui/core/styles';
 import { Grid } from "@material-ui/core";
+
+const useStyles = makeStyles((theme) => ({
+    root: {
+      flexGrow: 1,
+    }
+  }));
 
 
 export const getStaticProps = async() => {
@@ -32,14 +39,16 @@ export const getStaticProps = async() => {
 }
 
 export default function Resume(props) {
+    const classes = useStyles();
     return(
         <>  
             <Navbar/>
-            <Grid container spacing={2} direction="row" justify="center" alignItems="top" >
-                <Grid item xs={6}>
+            <div className={classes.root}>
+            <Grid container spacing={2} direction="row" justify="center" alignItems="flex-start" >
+                <Grid item xs={12} sm={6}>
                     <ExperienceSection {...props}/>
                 </Grid>
-                <Grid item xs={6} >
+                <Grid item  xs={12} sm={6}>
                     <Grid container spacing={2} direction="row" justify="center" alignItems="flex-start" >
                         <Grid item xs={12}>
                             <EducationSection {...props}/>
@@ -53,7 +62,7 @@ export default function Resume(props) {
                     </Grid>
                 </Grid>
             </Grid>
- 
+            </div>
         </>
     );
 
